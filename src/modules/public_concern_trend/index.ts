c57a7b25c3f2705397c5dfc37b36fabc017bd168
amel <amel@amel.local>
2023-11-02 17:40:39 +0800

    feat: upload pct
    Deskripsi:
    - backend upload
    - routing upload
    - validasi csv
    - modal upload
    
    No Issues

--- src/modules/public_concern_trend/index.ts
+++ src/modules/public_concern_trend/index.ts
@@ -1,7 +1,9 @@
 import funDownloadPctByArea from "./back/fun/download_pct_by_area";
 import funGetPctByArea from "./back/fun/get_pct_by_area";
 import ViewAdminPCT from "./back/view/view_admin_pct";
+import ViewUploadPct from "./back/view/view_upload_pct";
 
 export { ViewAdminPCT }
 export { funGetPctByArea }
 export { funDownloadPctByArea }
+export { ViewUploadPct }
