0bfd9982e463c16d026c531b831722a0f563da3a
amel <amel@amel.local>
2023-11-02 17:38:54 +0800

    feat: upload lta
    Deskripsi:
    - roouting upload
    - backend upload
    - modal konfirmasi
    - validasi csv
    
     No Issues

---
+++ src/modules/leader_trait_assessment/back/component/modal_upload_lta.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { Alert, Box, Button, Group, Text } from "@mantine/core"
+import { useAtom } from "jotai"
+import toast from "react-simple-toasts"
+import { isModalLta } from "../val/modal_lta"
+import funUploadLta from "../fun/upload_lta"
+
+export default function ModalUploadLta({ data, onSuccess }: { data: any, onSuccess: (val: any) => void }) {
+    const [openModal, setOpenModal] = useAtom(isModalLta)
+
+    async function onUpload() {
+        await funUploadLta({ body: data })
+        toast('Success', { theme: 'dark' })
+        setOpenModal(false)
+        onSuccess(true)
+    }
+
+    return (
+        <>
+            <Box>
+                <Alert color="gray" variant="outline">
+                    <Text fw={700} ta={"center"} mb={20} mt={20}>
+                        ANDA YAKIN INGIN MENGUPDATE DATA LEADER TRAIT ASSESSMENT?
+                    </Text>
+                    <Group justify="space-between" pt={10}>
+                        <Button
+                            radius={10}
+                            color="gray.7"
+                            w={150}
+                            onClick={() => setOpenModal(false)}
+                        >
+                            NO
+                        </Button>
+                        <Button radius={10} color="gray.7" w={150} onClick={() => onUpload()}>
+                            YES
+                        </Button>
+                    </Group>
+                </Alert>
+            </Box>
+        </>
+    )
+}
--- src/modules/leader_trait_assessment/back/view/view_upload_lta.tsx
+++ src/modules/leader_trait_assessment/back/view/view_upload_lta.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ButtonBack } from "@/modules/_global"
-import { Box, Button, Group, ScrollArea, Stack, Table, Text, rem } from "@mantine/core"
+import { Box, Button, Group, Modal, ScrollArea, Stack, Table, Text, rem } from "@mantine/core"
 import { Dropzone } from "@mantine/dropzone"
 import { useState } from "react"
 import { AiOutlineUpload } from "react-icons/ai"
@@ -9,20 +9,43 @@ import { MdCancelPresentation } from "react-icons/md"
 import { GrDocumentCsv } from 'react-icons/gr';
 import toast from "react-simple-toasts"
 import papa from 'papaparse'
+import { useAtom } from "jotai"
+import { isModalLta } from "../val/modal_lta"
+import ModalUploadLta from "../component/modal_upload_lta"
+
 
 export default function ViewUploadLTA() {
     const [json, setJson] = useState<any[]>([])
+    const [openModal, setOpenModal] = useAtom(isModalLta)
 
 
-    async function onpload(data: any) {
-        //   const res = await funUploadLeader(data)
-        //   if (res.success) return toast(res.message)
+    async function onLoad(data: any) {
+        if (data.length > 0) {
+            if (
+                ('id' in data[0]) &&
+                ('Provinsi' in data[0]) &&
+                ('Kabkot' in data[0]) &&
+                ('pekerjaKeras' in data[0]) &&
+                ('cerdas' in data[0]) &&
+                ('jujur' in data[0]) &&
+                ('merakyat' in data[0]) &&
+                ('tegas' in data[0]) &&
+                ('berpengalamanMemimpin' in data[0]) &&
+                ('berprestasi' in data[0]) &&
+                ('latarBelakangMiliter' in data[0]) &&
+                ('agamis' in data[0])
+            ) {
+                setJson(data as any)
+            } else {
+                setJson([])
+                toast('Format CSV salah', { theme: 'dark' })
+            }
+        } else {
+            setJson([])
+            toast('Data Kosong', { theme: 'dark' })
+        }
     }
-    const [dataUpload, setDataUplaod] = useState()
 
-    const getHeadings = () => {
-        return json
-    };
 
     return (
         <>
@@ -48,7 +71,7 @@ export default function ViewUploadLTA() {
                         onDrop={async (files: any) => {
                             const csv_file = Buffer.from(await files[0].arrayBuffer()).toString()
                             const { data } = papa.parse(csv_file, { header: true, })
-                            setJson(data as any)
+                            onLoad(data)
                         }}
                         onReject={(files: any) => {
                             toast("success")
@@ -87,51 +110,93 @@ export default function ViewUploadLTA() {
                             </div>
                         </Group>
                     </Dropzone>
-                    <Box style={{
-                        borderRadius: 10,
-                        paddingTop: 20
-                    }}>
+                    {
+                        (json.length > 0) && (
+                            <>
+                                <Box style={{
+                                    borderRadius: 10,
+                                    paddingTop: 20
+                                }}>
+                                    <Box
+                                        style={{
+                                            backgroundColor: "white",
+                                            padding: 10,
+                                            borderRadius: 10,
+                                        }}
+                                    >
 
-                        <Box
-                            style={{
-                                backgroundColor: "white",
-                                padding: 10,
-                                borderRadius: 10,
-                            }}
-                        >
-                            <ScrollArea>
-                                <Table
-                                    withTableBorder
-                                    withRowBorders={false}
-                                    horizontalSpacing="xl"
-                                >
-                                    <Table.Thead>
-                                        <Table.Tr
-                                            style={{
-                                                borderBottom: "1px solid #CED4D9",
-                                            }}
-                                        >
-                                            <Table.Th>No</Table.Th>
-                                            <Table.Th>Nama</Table.Th>
-                                        </Table.Tr>
-                                    </Table.Thead>
-                                    <Table.Tbody>
-                                        {json.map(home =>
-                                            <Table.Tr key={home.id}>
-                                                <Table.Td>{home.id}</Table.Td>
-                                                <Table.Td>{home.name}</Table.Td>
-                                            </Table.Tr>
-                                        )}
-                                    </Table.Tbody>
-                                </Table>
-                            </ScrollArea>
-                        </Box>
-                    </Box>
-                    <Group >
-                        <Button mt={20} fullWidth bg={"dark"}>UPLOAD</Button>
-                    </Group>
+
+                                        <ScrollArea>
+                                            <Table
+                                                withTableBorder
+                                                withRowBorders={false}
+                                                horizontalSpacing="xl"
+                                            >
+                                                <Table.Thead>
+                                                    <Table.Tr
+                                                        style={{
+                                                            borderBottom: "1px solid #CED4D9",
+                                                        }}
+                                                    >
+                                                        <Table.Th>ID</Table.Th>
+                                                        <Table.Th>Provinsi</Table.Th>
+                                                        <Table.Th>Kabupaten/Kota</Table.Th>
+                                                        <Table.Th>Pekerja Keras</Table.Th>
+                                                        <Table.Th>Cerdas</Table.Th>
+                                                        <Table.Th>Jujur</Table.Th>
+                                                        <Table.Th>Merakyat</Table.Th>
+                                                        <Table.Th>Tegas</Table.Th>
+                                                        <Table.Th>Berpengalaman Memimpin</Table.Th>
+                                                        <Table.Th>Berprestasi</Table.Th>
+                                                        <Table.Th>Latar Belakang Militer</Table.Th>
+                                                        <Table.Th>Agamis</Table.Th>
+                                                    </Table.Tr>
+                                                </Table.Thead>
+                                                <Table.Tbody>
+                                                    {json.map(home =>
+                                                        <Table.Tr key={home.id}>
+                                                            <Table.Td>{home.id}</Table.Td>
+                                                            <Table.Td>{home.Provinsi}</Table.Td>
+                                                            <Table.Td>{home.Kabkot}</Table.Td>
+                                                            <Table.Td>{home.pekerjaKeras}</Table.Td>
+                                                            <Table.Td>{home.cerdas}</Table.Td>
+                                                            <Table.Td>{home.jujur}</Table.Td>
+                                                            <Table.Td>{home.merakyat}</Table.Td>
+                                                            <Table.Td>{home.tegas}</Table.Td>
+                                                            <Table.Td>{home.berpengalamanMemimpin}</Table.Td>
+                                                            <Table.Td>{home.berprestasi}</Table.Td>
+                                                            <Table.Td>{home.latarBelakangMiliter}</Table.Td>
+                                                            <Table.Td>{home.agamis}</Table.Td>
+                                                        </Table.Tr>
+                                                    )}
+                                                </Table.Tbody>
+                                            </Table>
+                                        </ScrollArea>
+
+                                    </Box>
+                                </Box>
+                                <Group >
+                                    <Button mt={20} fullWidth bg={"dark"} onClick={() => {
+                                        setOpenModal(true)
+                                    }}>UPLOAD</Button>
+                                </Group>
+                            </>
+                        )
+                    }
                 </Box>
             </Stack>
+
+            <Modal
+                opened={openModal}
+                onClose={() => setOpenModal(false)}
+                centered
+                withCloseButton={false}
+                closeOnClickOutside={false}
+            >
+                <ModalUploadLta data={json} onSuccess={(val) => {
+                    setJson([])
+                }} />
+            </Modal>
         </>
     );
 }
