352c8b6cff1f44b1a931aaadb65a550ede898631
lukman <lukman@lukmans-MacBook-Air.local>
2023-11-06 16:39:42 +0800

    style : tambah jokowi
    Deskripsi:
    
    - tambah view jokowi effect
    
    No Issue

--- src/modules/jokowi_effect/index.ts
+++ src/modules/jokowi_effect/index.ts
@@ -2,8 +2,10 @@ import funDownloadEffectByDate from "./back/fun/download_effect_by_date";
 import funGetEffectByDate from "./back/fun/get_effect_by_date";
 import ViewAdminJokowi from "./back/view/view_admin_jokowi";
 import ViewUploadJokowi from "./back/view/view_upload_jokowi";
+import ViewJokowiEffect from "./front/view/view_jokowi_effect";
 
 export { ViewAdminJokowi }
 export { funGetEffectByDate }
 export { funDownloadEffectByDate }
 export { ViewUploadJokowi }
+export {ViewJokowiEffect}
