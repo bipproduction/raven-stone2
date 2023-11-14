75d1f8e9d875da26dec7bc174c670673c3a63213
lukman <lukman@lukmans-MacBook-Air.local>
2023-11-14 17:50:58 +0800

    style : update app
    Deskripsi:
    - update dahsboard admin
    
    No Issue

---
+++ src/app/dashboard-admin/jokowi-effect/add/page.tsx
@@ -0,0 +1,10 @@
+import { ViewAddAdminJokowi } from '@/modules/jokowi_effect';
+import React from 'react';
+
+function Page() {
+  return (
+    <ViewAddAdminJokowi/>
+  );
+}
+
+export default Page;
