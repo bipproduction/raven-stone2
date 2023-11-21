e02f7fcce70b7a7b0f1b519088952644be54af3f
amel <amel@amel.local>
2023-11-21 22:13:34 +0800

    upd : global pwd

--- src/app/dashboard/(v1)/layout.tsx
+++ src/app/dashboard/(v1)/layout.tsx
@@ -5,12 +5,11 @@ import { redirect } from "next/navigation";
 import { funCekAkses } from "@/modules/_global";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-    // TODO : UNCOMMENT this
-    // const c = cookies().get("_tknRV")
-    // if (!c || !c.value || _.isEmpty(c.value)) return redirect('/')
+    const c = cookies().get("_tknRV")
+    if (!c || !c.value || _.isEmpty(c.value)) return redirect('/')
 
-    // const cekAkses = await funCekAkses()
-    // if(!cekAkses.dashboardUser && cekAkses.dashboardAdmin)  return redirect('/dashboard-admin/emotion-candidate')
+    const cekAkses = await funCekAkses()
+    if(!cekAkses.dashboardUser && cekAkses.dashboardAdmin)  return redirect('/dashboard-admin/emotion-candidate')
 
     return (
         <>
