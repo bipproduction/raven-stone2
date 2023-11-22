415460352f9b0d50a169e964f7d68232dc0cfc41
lukman <lukman@lukmans-MacBook-Air.local>
2023-11-22 12:46:41 +0800

    feat : update app dahsboard live
    Deskripsi:
    - update app dashboard live
    
    NO Issue:

--- src/app/dashboard-admin/(dashboard-live)/persen/[paslon]/page.tsx
+++ src/app/dashboard-admin/(dashboard-live)/persen/[paslon]/page.tsx
@@ -1,9 +1,12 @@
+import { funGetAllPaslon } from "@/modules/_global";
 import { EditPersen } from "@/modules/dashboard_live";
+import funGetOnePersen from "@/modules/dashboard_live/back/fun/get_one_persen";
 
-export default function Page({ params }: { params: { paslon: string } }) {
+export default async function Page({ params }: { params: { paslon: string } }) {
+    const data = await funGetOnePersen({id: params.paslon})
     return (
         <>
-        <EditPersen data={params.paslon}/>
+        <EditPersen data={data}/>
         </>
     )
 }
