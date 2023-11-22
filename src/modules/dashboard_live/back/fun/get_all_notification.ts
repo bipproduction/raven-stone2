85d4ba8de157d2b43ea37126ffc2b1f77f2828d8
amel <amel@amel.local>
2023-11-22 12:00:10 +0800

    upd: setting live dashboard
    Deskripsi:
    - fixed load saat delete notif
    
    No Issues

--- src/modules/dashboard_live/back/fun/get_all_notification.ts
+++ src/modules/dashboard_live/back/fun/get_all_notification.ts
@@ -2,7 +2,7 @@
 
 import { prisma } from "@/modules/_global"
 
-export default async function  funGetAllNotification() {
+export default async function funGetAllNotification() {
     const data = await prisma.liveDashboardNotif.findMany({
         where: {
             isActive: true,
