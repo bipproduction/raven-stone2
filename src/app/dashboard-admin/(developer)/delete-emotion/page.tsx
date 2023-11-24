9833351e548d07778376df182a93bd03d651ad3f
lukman <lukman@lukmans-MacBook-Air.local>
2023-11-24 20:24:07 +0800

    feat : update app delete emotion
    Deskripsi:
    - update app emotion delete candidate
    
    No Issue

--- src/app/dashboard-admin/(developer)/delete-emotion/page.tsx
+++ src/app/dashboard-admin/(developer)/delete-emotion/page.tsx
@@ -1,19 +1,20 @@
-import { funGetAllPaslon } from '@/modules/_global';
+import { funGetAllCandidate, funGetAllPaslon } from '@/modules/_global';
 import { ViewDeleteEmotion, funGetEmotionCandidateDateArea } from '@/modules/emotion';
 import funGetAllEmotionPaslon from '@/modules/emotion/back/fun/get_all_emotion_paslon';
 import _ from 'lodash';
 import React from 'react';
 
-export default async function Page({ searchParams }: { searchParams: { paslon: any,  } }) {
+export default async function Page({ searchParams }: { searchParams: { paslon: any, candidate: any  } }) {
   const findData = {
     idPaslon: (_.isNaN(Number(searchParams.paslon)) ? 1 : Number(searchParams.paslon)),
     // date: (_.isUndefined(searchParams.date) ? new Date() : new Date(searchParams.date)),
   }
 
   const dataPaslon = await funGetAllPaslon()
+  const dataCandidate = await funGetAllCandidate()
   return (
     <>
-      <ViewDeleteEmotion paslon={dataPaslon}  />
+      <ViewDeleteEmotion paslon={dataPaslon} candidate={dataCandidate}  />
     </>
   );
 }
