9bf6692bb05208bc584b4bcb21e36d5ab72e7feb
amel <amel@amel.local>
2023-11-29 15:09:12 +0800

    doc: tambah dokumentasi
    Deskripsi:
    - menambahkan dokumentasi pada file di module audience
    
    No Issues

--- src/modules/audience/back/fun/get_audience_by_area.ts
+++ src/modules/audience/back/fun/get_audience_by_area.ts
@@ -1,15 +1,23 @@
 'use server'
-
 import { countProvince, prisma } from "@/modules/_global";
 import _ from "lodash"
+
+/**
+ * Get data audience berdasarkan area yg dipilih
+ * @param find array yang berisikan id provinsi
+ * @returns array judul dan data audience
+ */
 
 export default async function funGetAudienceByArea({ find }: { find: any }) {
     let titleTrue, dataTable = <any>[], area, th
 
+    // get jumlah provinsi
     const nProv = await countProvince();
 
     if (find.idProvinsi > 0 && find.idProvinsi <= nProv) {
 
+        // jika parameter id provinsi > 0 dan <= jumlah provinsi,
+        // maka get data audience berdasarkan idprovinsi
         dataTable = await prisma.audience.findMany({
             where: {
                 idProvinsi: find.idProvinsi
@@ -30,15 +38,20 @@ export default async function funGetAudienceByArea({ find }: { find: any }) {
             name: v.AreaKabkot.name
         }))
 
+        // get one data provinsi sesuai dg id
         area = await prisma.areaProvinsi.findUnique({
             where: {
                 id: find.idProvinsi
             }
         })
+
+        // set judul
         titleTrue = "PROVINSI " + area?.name
         th = "KABUPATEN/KOTA"
 
     } else {
+
+        // selain itu maka get all data audience (tanpa kondisi)
         dataTable = await prisma.audience.findMany({
             select: {
                 value: true,
@@ -57,10 +70,13 @@ export default async function funGetAudienceByArea({ find }: { find: any }) {
             name: v.AreaProvinsi.name
         }))
 
+        // menjumlahkan value audience berdasarkan idProvinsi
         dataTable = _.map(_.groupBy(dataTable, "idProvinsi"), (v: any) => ({
             value: _.sumBy(v, 'value'),
             name: v[0].name
         }))
+
+        // set judul
         titleTrue = "SELURUH PROVINSI"
         th = "PROVINSI"
     }
--- src/modules/audience/front/fun/get_all_locked_audience_by_prov.ts
+++ src/modules/audience/front/fun/get_all_locked_audience_by_prov.ts
@@ -1,6 +1,11 @@
 'use server'
-
 import { prisma } from "@/modules/_global"
+
+/**
+ * Get data locked audience kabkot berdasarkan id provinsi
+ * @param provinsi id provinsi
+ * @returns array data locked audience
+ */
 
 export default async function funGetAudienceByProvFront({ provinsi }: { provinsi: any }) {
 
