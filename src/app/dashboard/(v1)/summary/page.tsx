6b08fbb0cafa7cbe30200eac492c9d29f9920b76
lukman <lukman@lukmans-MacBook-Air.local>
2023-12-11 17:29:17 +0800

    feat : update summary
    Deskripsi:
    - update page summary
    
    No Issue

--- src/app/dashboard/(v1)/summary/page.tsx
+++ src/app/dashboard/(v1)/summary/page.tsx
@@ -1,9 +1,22 @@
 import { ViewSummary } from "@/modules/emotion";
+import { funGetAllAudienceFront } from "@/modules/audience";
+import { funGetEmotionCandidateChartFront, funGetEmotionJokowiEffectAreaFront, funGetEmotionPersenJokowiFront } from "@/modules/emotion";
+import { ViewJokowiEffect, funGetEffectFront } from "@/modules/jokowi_effect";
+import moment from "moment";
 
 export default async function Page() {
+    const dEffect = await funGetEffectFront({ isDate: new Date() })
+    const dEmotion = await funGetEmotionJokowiEffectAreaFront()
+    const dLocked = await funGetAllAudienceFront()
+    const dEmotionPersen = await funGetEmotionPersenJokowiFront()
+    const dEmotionChart = await funGetEmotionCandidateChartFront({
+        candidate: 7,
+        startDate: moment(new Date()).subtract(7, "days").format("YYYY-MM-DD"),
+        endDate: moment(new Date()).format("YYYY-MM-DD")
+    })
     return (
         <>
-        <ViewSummary/>
+        <ViewSummary effect={dEffect} emotion={dEmotion} locked={dLocked} persen={dEmotionPersen} emotionChart={dEmotionChart} />
         </>
     )
 }
