a8f4c4c1f23b4321480500a124aeaecce95aa941
amel <amel@amel.local>
2023-12-22 11:32:34 +0800

    upd : fix qc
    Deskripsi:
    - memperbaiki server action get data popularity (pengambilan data terakhir)
    
    No Issues

--- src/modules/emotion/front/fun/get_emotion_regional_front.ts
+++ src/modules/emotion/front/fun/get_emotion_regional_front.ts
@@ -1,6 +1,7 @@
 'use server'
 import { prisma } from "@/modules/_global"
 import _ from "lodash"
+import moment from "moment"
 
 /**
  * Fungsi untuk get emotion regional front.
@@ -10,68 +11,99 @@ import _ from "lodash"
  */
 
 export default async function funGetEmotionRegionalFront({ candidate, region }: { candidate: any, region?: any }) {
+    const jamNow = new Date().getHours() + 1 + ':00:00'
+    const IniisoDateTime = new Date(new Date('1970-01-01 ' + jamNow).getTime() - (new Date('1970-01-01 ' + jamNow).getTimezoneOffset() * 60000)).toISOString()
     let kondisi
 
-    if (_.isUndefined(region) || _.isNull(region)) {
-        kondisi = {
-            idCandidate: Number(candidate),
-            dateEmotion: new Date()
-        }
-    } else {
-        kondisi = {
+    const dataJam = await prisma.candidateEmotion.findMany({
+        where: {
             idCandidate: Number(candidate),
             dateEmotion: new Date(),
-            idProvinsi: Number(region)
-        }
-    }
-
-    const data = await prisma.candidateEmotion.findMany({
-        where: kondisi,
-        select: {
-            confidence: true,
-            dissapproval: true,
-            negative: true,
-            positive: true,
-            supportive: true,
-            uncomfortable: true,
-            undecided: true,
-            unsupportive: true,
-            idProvinsi: true,
-            AreaProvinsi: {
-                select: {
-                    name: true,
-                }
+            timeEmotion: {
+                lt: IniisoDateTime
             }
+        },
+        orderBy: {
+            timeEmotion: 'desc'
+        },
+        select: {
+            timeEmotion: true,
+            idCandidate: true,
         }
     })
 
-    const format = data.map((v: any) => ({
-        ..._.omit(v, ["AreaProvinsi"]),
-        name: v.AreaProvinsi.name
+    const findJam = _.map(_.groupBy(dataJam, ["timeEmotion"]), (v: any) => ({
+        timeEmotionFormat: moment.utc(v[0].timeEmotion).format('HH:mm'),
+        timeEmotion: v[0].timeEmotion
     }))
 
-    const result = _.map(_.groupBy(format, "idProvinsi"), (v: any) => ({
-        name: _.toString(v[0].name),
-        idProvinsi: v[0].idProvinsi,
-        confidence: _.sumBy(v, 'confidence'),
-        dissapproval: _.sumBy(v, 'dissapproval'),
-        negative: _.sumBy(v, 'negative'),
-        positive: _.sumBy(v, 'positive'),
-        supportive: _.sumBy(v, 'supportive'),
-        uncomfortable: _.sumBy(v, 'uncomfortable'),
-        undecided: _.sumBy(v, 'undecided'),
-        unsupportive: _.sumBy(v, 'unsupportive'),
-        filtered: _.sum([
-            _.sumBy(v, 'confidence'),
-            _.sumBy(v, 'dissapproval'),
-            _.sumBy(v, 'negative'),
-            _.sumBy(v, 'positive'),
-            _.sumBy(v, 'supportive'),
-            _.sumBy(v, 'uncomfortable'),
-            _.sumBy(v, 'undecided'),
-            _.sumBy(v, 'unsupportive'),
-        ])
-    }))
+    if (findJam.length > 0) {
+        if (_.isUndefined(region) || _.isNull(region)) {
+            kondisi = {
+                idCandidate: Number(candidate),
+                dateEmotion: new Date()
+            }
+        } else {
+            kondisi = {
+                idCandidate: Number(candidate),
+                dateEmotion: new Date(),
+                idProvinsi: Number(region)
+            }
+        }
 
-    return result
+        const data = await prisma.candidateEmotion.findMany({
+            where: kondisi,
+            select: {
+                confidence: true,
+                dissapproval: true,
+                negative: true,
+                positive: true,
+                supportive: true,
+                uncomfortable: true,
+                undecided: true,
+                unsupportive: true,
+                idProvinsi: true,
+                AreaProvinsi: {
+                    select: {
+                        name: true,
+                    }
+                }
+            }
+        })
+
+        const format = data.map((v: any) => ({
+            ..._.omit(v, ["AreaProvinsi"]),
+            name: v.AreaProvinsi.name
+        }))
+
+        const result = _.map(_.groupBy(format, "idProvinsi"), (v: any) => ({
+            name: _.toString(v[0].name),
+            idProvinsi: v[0].idProvinsi,
+            confidence: _.sumBy(v, 'confidence'),
+            dissapproval: _.sumBy(v, 'dissapproval'),
+            negative: _.sumBy(v, 'negative'),
+            positive: _.sumBy(v, 'positive'),
+            supportive: _.sumBy(v, 'supportive'),
+            uncomfortable: _.sumBy(v, 'uncomfortable'),
+            undecided: _.sumBy(v, 'undecided'),
+            unsupportive: _.sumBy(v, 'unsupportive'),
+            filtered: _.sum([
+                _.sumBy(v, 'confidence'),
+                _.sumBy(v, 'dissapproval'),
+                _.sumBy(v, 'negative'),
+                _.sumBy(v, 'positive'),
+                _.sumBy(v, 'supportive'),
+                _.sumBy(v, 'uncomfortable'),
+                _.sumBy(v, 'undecided'),
+                _.sumBy(v, 'unsupportive'),
+            ])
+        }))
+        
+        return result
+
+    } else {
+        return []
+    }
+
+
 }
--- src/modules/national_popularity_metric/front/fun/get_rate.ts
+++ src/modules/national_popularity_metric/front/fun/get_rate.ts
@@ -1,23 +1,44 @@
 'use server'
-
 import { prisma } from "@/modules/_global"
-
+import _ from "lodash"
 
 /**
  * Fungsi untuk get rate front.
  * @param {paslon} paslon - menampilkan paslon.
  * @returns Untuk get rate front
  */
+
 export default async function funGetRateFront({ paslon }: { paslon: any }) {
-    const data = await prisma.paslonPopularity.findFirst({
+    const jamNow = new Date().getHours() + 1 + ':00:00'
+    const IniisoDateTime = new Date(new Date('1970-01-01 ' + jamNow).getTime() - (new Date('1970-01-01 ' + jamNow).getTimezoneOffset() * 60000)).toISOString()
+
+    const dataJam = await prisma.paslonPopularity.findFirst({
         where: {
+            dateEmotion: new Date(),
             idPaslon: Number(paslon),
-            dateEmotion: new Date()
+            timeEmotion: {
+                lt: IniisoDateTime
+            }
         },
-        select: {
-            rate: true
-        }
+        orderBy: {
+            timeEmotion: 'desc'
+        },
     })
+
+    let data: any = null
+
+    if (!_.isNull(dataJam)) {
+        data = await prisma.paslonPopularity.findFirst({
+            where: {
+                idPaslon: Number(paslon),
+                dateEmotion: new Date(),
+                timeEmotion: dataJam.timeEmotion
+            },
+            select: {
+                rate: true
+            }
+        })
+    }
 
     return data
 }
